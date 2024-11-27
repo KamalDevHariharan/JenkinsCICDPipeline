7d89d0bc75bc91bd8bc774a2a9ae8deb497298c1
KamaldevHariharan <contact.kamal96@gmail.com>
2024-11-28 00:57:01 +0530

    test file updated

--- src/test/java/com/kamal/controller/BasicMathCheckTest.java
+++ src/test/java/com/kamal/controller/BasicMathCheckTest.java
@@ -1,6 +1,6 @@
 package com.kamal.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BasicMathCheckTest {
@@ -8,24 +8,24 @@ public class BasicMathCheckTest {
     @Test
     public void Addition() {
         BasicMathCheckTest test = new BasicMathCheckTest();
-        assertEquals("The output should be 7", 7, test.addition(5,2));
+        Assert.assertEquals("expected value: 7", 7, test.addition(5,2));
     }
 
     @Test
     public void Subtraction() {
-        BasicMathCheckTest test = new BasicMathCheckTest();
-        assertEquals("The output should be 3", 3, test.subtraction(5,2));
+        BasicMathCheck test = new BasicMathCheck();
+        Assert.assertEquals("expected value: 3", 3, test.subtraction(5,2));
     }
 
     @Test
     public void Multiplication() {
-        BasicMathCheckTest test = new BasicMathCheckTest();
-        assertEquals("The output should be 6", 6, test.multiplication(2,3));
+        BasicMathCheck test = new BasicMathCheck();
+        Assert.assertEquals("expected value: 6", 6, test.multiplication(2,3));
     }
 
     @Test
     public void Division() {
-        BasicMathCheckTest test = new BasicMathCheckTest();
-        assertEquals("The output should be 2", 2, test.division(8,4));
+        BasicMathCheck test = new BasicMathCheck();
+        Assert.assertEquals("expected value: 2", 2, test.division(8,4));
     }
 }
