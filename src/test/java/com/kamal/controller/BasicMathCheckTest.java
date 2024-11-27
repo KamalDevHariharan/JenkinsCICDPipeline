99926f81faefb7559d025d83dc3f156c1a0640fb
KamaldevHariharan <contact.kamal96@gmail.com>
2024-11-28 00:44:43 +0530

    file updated

--- src/test/java/com/kamal/controller/BasicMathCheckTest.java
+++ src/test/java/com/kamal/controller/BasicMathCheckTest.java
@@ -7,25 +7,25 @@ public class BasicMathCheckTest {
 
     @Test
     public void Addition() {
-        MyCalculator test = new MyCalculator();
+        BasicMathCheckTest test = new BasicMathCheckTest();
         assertEquals("The output should be 7", 7, test.addition(5,2));
     }
 
     @Test
     public void Subtraction() {
-        MyCalculator test = new MyCalculator();
+        BasicMathCheckTest test = new BasicMathCheckTest();
         assertEquals("The output should be 3", 3, test.subtraction(5,2));
     }
 
     @Test
     public void Multiplication() {
-        MyCalculator test = new MyCalculator();
+        BasicMathCheckTest test = new BasicMathCheckTest();
         assertEquals("The output should be 6", 6, test.multiplication(2,3));
     }
 
     @Test
     public void Division() {
-        MyCalculator test = new MyCalculator();
+        BasicMathCheckTest test = new BasicMathCheckTest();
         assertEquals("The output should be 2", 2, test.division(8,4));
     }
 }
